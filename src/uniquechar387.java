040a749141cf4f9d23bb451d37517cfd54e47cb8
Kanha gupta <kanhag4163@gmail.com>
2022-08-05 12:47:15 +0530

    387

---
+++ src/uniquechar387.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class uniquechar387 {
+    public static void main(String[] args) {
+        System.out.println(uniquechar("leetcode"));
+    }
+    static int uniquechar(String s){
+        HashMap<Character,Integer> map= new HashMap<Character,Integer>();
+        for (int i = 0; i < s.length() ; i++) {
+            char current = s.charAt(i);
+            if(!map.containsKey(current)){
+                map.put(current,i);
+            }
+            else {
+                map.put(current,-1);
+            }
+        }
+        int min=Integer.MAX_VALUE;
+        for(char c: map.keySet()){
+            if(map.get(c)> -1 && map.get(c)< min){
+                min=map.get(c);
+            }
+        }
+        return min ==Integer.MAX_VALUE ? -1:min;
+    }
+}
