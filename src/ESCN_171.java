4ba2aeed4fe7e5d270bd47bc03412da8b6ac1704
Kanha gupta <kanhag4163@gmail.com>
2022-08-05 14:23:14 +0530

    -

---
+++ src/ESCN_171.java
@@ -0,0 +1,13 @@
+public class ESCN_171 {
+    public static void main(String[] args) {
+        System.out.println(excelsheetcolumnumber("ZA"));
+    }
+    static int excelsheetcolumnumber(String s){
+        int result=0;
+        for (int i = 0; i <s.length(); i++) {
+            result=result*26;
+            result=result+s.charAt(i)-'A'+1;
+        }
+        return result;
+    }
+}
