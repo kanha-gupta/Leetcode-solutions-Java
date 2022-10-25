e5e54348930114417976de7d131a6983d8872cd2
Kanha gupta <kanhag4163@gmail.com>
2022-10-25 23:53:21 +0530

    No1

--- src/No1_twosum.java
+++ src/No1_twosum.java
@@ -5,15 +5,17 @@ import java.util.Map;
 public class No1_twosum {
     public static void main(String[] args) {
         int[] arr={2,7,11,15};
-        int target=8;
+        int target=26;
         System.out.println(Arrays.toString(twoSum(arr,target)));
     }
     static int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i =0; i< nums.length; i++) {
+            //if condition checks whether map contains the other value of sum. if it doesnt, else condition is applied
             if(map.containsKey(target - nums[i])){
                 return new int[]{map.get(target - nums[i]), i};
             }else {
+                //it is filling the map one by one if it doesnt gets the required answer.
                 map.put(nums[i], i);
             }
         }
