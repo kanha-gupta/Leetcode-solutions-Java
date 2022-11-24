decaa7fd067cd22239a9cc4685f682e9217e9d77
Kanha gupta <kanhag4163@gmail.com>
2022-11-24 15:31:24 +0530

    -

---
+++ src/No268_missingNumber.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class No268_missingNumber {
+//Set approach. Time complexity is average
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) return i;
+        }
+        return 0;
+    }
+    //Mathematical approach. Excellent time complexity
+    public int missingNumber2(int[] nums){
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum=sum+nums[i];
+        }
+        //it applies the logic of sum of natural numbers & substract the sum with it
+        //to get the answer. using nums.length
+        return nums.length*(nums.length+1)/2-sum;
+
+    }
+}
