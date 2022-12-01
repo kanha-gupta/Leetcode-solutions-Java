148ceae5ed33b8a6c2a9ef0bc645ffdc4602d9b8
Kanha gupta <kanhag4163@gmail.com>
2022-12-01 22:17:53 +0530

    -

--- src/LeetcodeDailyChallenge/No1704_StringsHalvesAlike.java
+++ src/LeetcodeDailyChallenge/No1704_StringsHalvesAlike.java
@@ -1,12 +1,10 @@
+package LeetcodeDailyChallenge;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class No1704_StringsHalvesAlike {
-    public static void main(String[] args) {
-        halvesAreAlike("book");
-        System.out.println("book".length());
-    }
-    static boolean halvesAreAlike(String s) {
+    public boolean halvesAreAlike(String s) {
         Map<Character,Integer> hashmap=new HashMap<>();
         hashmap.put('a',1);
         hashmap.put('e',1);
@@ -19,6 +17,7 @@ public class No1704_StringsHalvesAlike {
         hashmap.put('O',1);
         hashmap.put('U',1);
         //we'll count no. of vowels using for loops
+        //Time complexity O(N)
         int firsthalfcounter=0;
         int secondhalfcounter=0;
         for (int i = 0; i < s.length()/2; i++) {
