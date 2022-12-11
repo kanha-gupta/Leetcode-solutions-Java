da6166eb6b1e5f5aa618d7296be9c0cfa68fe850
Kanha gupta <kanhag4163@gmail.com>
2022-12-11 16:58:09 +0530

    -

--- src/No160_getIntersectNode.java
+++ src/No160_getIntersectNode.java
@@ -13,41 +13,47 @@ public class No160_getIntersectNode {
         if(headA==headB){
             return headA;
         }
-            ListNode a=headA;
-            ListNode b=headB;
-            int len1=1;
-            int len2=1;
-            //counting the length
-            while (a.next!=null){
-                len1++;
+        ListNode a=headA;
+        ListNode b=headB;
+        int len1=1;
+        int len2=1;
+        //counting the length
+        while (a.next!=null){
+            len1++;
+            a=a.next;
+        }
+        while (b.next!=null){
+            len2++;
+            b=b.next;
+        }
+        //after counting the length we will move pointers to make length same
+        a=headA;
+        b=headB;
+        while (a!=null && b!=null){
+            if(len1>len2){
+                if(a==b){
+                    return a;
+                }
                 a=a.next;
+                len1--;
             }
-            while (b.next!=null){
-                len2++;
+            if(len2>len1){
+                if(a==b){
+                    return b;
+                }
+                b=b.next;
+                len2--;
+            }
+            if(len1==len2){
+                if(a==b){
+                    return a;
+                }
+                a=a.next;
                 b=b.next;
             }
-            //after counting the length we will move pointers to make length same
-            a=headA;
-            b=headB;
-            int difference;
-                if(len1>len2){
-                    difference=len1-len2;
-                    for (int i = 0; i < difference; i++) {
-                        a=a.next;
-                    }
-                }
-                if(len2>len1){
-                   difference=len2-len1;
-                    for (int i = 0; i < difference; i++) {
-                        b=b.next;
-                    }
-                }
-               while (a!=b){
-                   a=a.next;
-                   b=b.next;
-               }
-            return a;
         }
+        return null;
+    }
     }
 
 
