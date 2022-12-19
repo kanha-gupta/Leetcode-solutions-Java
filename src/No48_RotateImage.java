0872b3c4703371b93b6eee763dfed169b9c8e40c
Kanha gupta <kanhag4163@gmail.com>
2022-12-19 21:55:06 +0530

    -

--- src/No48_RotateImage.java
+++ src/No48_RotateImage.java
@@ -1,19 +1,28 @@
 public class No48_RotateImage {
     //we divide in it 2 parts, transpose & reflect
+
     public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
     }
-    public void transpose(int[][] matrix){
+
+    public void transpose(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = i+1; j <matrix.length ; j++) {
-                int temp=matrix[i][j];
-                matrix[j][i]= matrix[i][j];
-                matrix[i][j]= temp;
+            for (int j = i + 1; j < matrix.length; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
             }
         }
     }
-    public void reflect(int[][] matrix){
-        for (int i = 0; i < ; i++) {
-            
+
+    public void reflect(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = tmp;
+            }
         }
     }
 }
