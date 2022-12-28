fdcbe2c0c25a3961e0d473e8a22f71544e002597
Kanha gupta <kanhag4163@gmail.com>
2022-12-28 17:10:55 +0530

    -

--- src/No55_JumpGame.java
+++ src/No55_JumpGame.java
@@ -1,15 +1,13 @@
 public class No55_JumpGame {
-    public static void main(String[] args) {
-        int[] arr={3,2,1,0,4};
-        canJump(arr);
-    }
-    static boolean canJump(int[] nums) {
-        int reachable = 0;
+    public boolean canJump(int[] nums) {
+        int MaxJumpReach = 0;
         for (int i=0; i<nums.length; ++i) {
-            if (i > reachable){
+            //the main thing to care about is that i shouldnt get greater than MaxJumpReach because if it does,
+            //it will mean that we cannot reach to the last index & code should terminate
+            if (i > MaxJumpReach){
                 return false;
             }
-            reachable = Math.max(reachable, i + nums[i]);
+            MaxJumpReach = Math.max(MaxJumpReach, i + nums[i]);
         }
         return true;
     }
