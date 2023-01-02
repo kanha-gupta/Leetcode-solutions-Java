f2eb0151179fdd30df5799d5e2768d52c394af07
Kanha gupta <kanhag4163@gmail.com>
2023-01-03 01:38:11 +0530

    -

---
+++ src/No290_WordPattern.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class No290_WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        //we make 2 maps to verify the pattern. if one of them fails, return false
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (!charToWord.containsKey(c)) {
+                charToWord.put(c, word);
+            }
+            if (!wordToChar.containsKey(word)) {
+                wordToChar.put(word, c);
+            }
+            if (!charToWord.get(c).equals(word) || !wordToChar.get(word).equals(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
