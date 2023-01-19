d8b63a80a77e12fbff5b40d93b85c035744837f1
Kanha gupta <kanhag4163@gmail.com>
2023-01-20 00:01:33 +0530

    -

---
+++ src/No198_HouseRobber.java
@@ -0,0 +1,16 @@
+public class No198_HouseRobber {
+    public int rob(int[] nums) {
+      int n=nums.length;
+      int[] dp=new int[n];
+      dp[0]=nums[0];
+        for (int i = 1; i < n; i++) {
+            if(i>=2){
+                dp[i]=Math.max(dp[i],Math.max(nums[i]+dp[i-2],dp[i-1]));
+            }
+            else {
+                dp[i]=Math.max(nums[i],dp[i-1]);
+            }
+        }
+        return dp[n-1];
+    }
+}
