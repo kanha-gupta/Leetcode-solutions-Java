0af649c77c5d2f722356f98cddf8eb5621cf5c1b
Kanha gupta <kanhag4163@gmail.com>
2023-01-23 21:57:23 +0530

    -

--- src/No56_MergeIntervals.java
+++ src/No56_MergeIntervals.java
@@ -1,14 +1,5 @@
 public class No56_MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        int[][] result=new int[intervals.length][intervals[0].length];
-        for (int i = 0; i<intervals.length ; i++) {
-            for (int j = 0; j <intervals[0].length; j++) {
-               result[i][j]=intervals[i][j];
-               if(intervals[i][j]< intervals[i][j+1] ||
-                       intervals[i][j]<intervals[i+1][j-1]);
-                {
-                }
-            }
-        }
+
     }
 }
