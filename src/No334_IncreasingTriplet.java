ac123a9435a88db6eca45cf17fe608706ac93ab1
Kanha gupta <kanhag4163@gmail.com>
2023-01-24 01:44:41 +0530

    -

---
+++ src/No334_IncreasingTriplet.java
@@ -0,0 +1,22 @@
+public class No334_IncreasingTriplet {
+
+    public boolean increasingTriplet(int[] nums) {
+        if(nums==null || nums.length<3){
+            return false;
+        }
+        int minOne=Integer.MAX_VALUE;
+        int minTwo=Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]<minOne){
+                minOne=nums[i];
+            }
+            if(nums[i]>minOne){
+                minTwo=Math.min(nums[i],minTwo);
+            }
+            if(nums[i]>minTwo){
+                return true;
+            }
+        }
+        return false;
+    }
+}
