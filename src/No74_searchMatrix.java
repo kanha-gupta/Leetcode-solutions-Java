4663b9fca222d4ba43beed0e39b2606d675830e5
Kanha gupta <kanhag4163@gmail.com>
2023-02-05 01:29:03 +0530

    -

---
+++ src/No74_searchMatrix.java
@@ -0,0 +1,21 @@
+public class No74_searchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix[0].length;
+        int n=matrix.length;
+        int low=0;
+        int high=m*n-1;
+        while (low<=high){
+            int mid= low+ (high-low)/2;
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target){
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
