1691656fa90a1de646cd2ed9552d4f0e2a389a0d
Kanha gupta <kanhag4163@gmail.com>
2023-03-06 21:07:18 +0530

    -

--- src/No1_twosum.java
+++ src/No1_twosum.java
@@ -3,12 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class No1_twosum {
-    public static void main(String[] args) {
-        int[] arr={2,7,11,15};
-        int target=26;
-        System.out.println(Arrays.toString(twoSum(arr,target)));
-    }
-    static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i =0; i< nums.length; i++) {
             //if condition checks whether map contains the other value of sum. if it doesnt, else condition is applied
