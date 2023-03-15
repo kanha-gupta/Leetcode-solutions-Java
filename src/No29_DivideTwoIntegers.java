aa052edc14c342621de7a0d78cccaa2eab36f5d2
Kanha gupta <kanhag4163@gmail.com>
2023-03-15 19:41:04 +0530

    -

---
+++ src/No29_DivideTwoIntegers.java
@@ -0,0 +1,9 @@
+public class No29_DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        if (dividend == -2147483648 && divisor == -1) {
+            return (2147483647);
+        }
+        int result=dividend/divisor;
+        return result;
+    }
+}
