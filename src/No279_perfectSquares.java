d7ef47f2aeeca2ef4fcc8b303f91faa38dd407d0
Kanha gupta <kanhag4163@gmail.com>
2023-08-05 07:27:14 +0530

    -

---
+++ src/No279_perfectSquares.java
@@ -0,0 +1,18 @@
+public class No279_perfectSquares {
+    public static void main(String[] args) {
+        numSquares(80);
+    }
+    static int numSquares(int n) {
+        int[] dp = new int[n+1];
+        dp[0]=0;
+
+        for (int i = 1; i <=n ; i++) {
+            dp[i]=i;
+            for (int j = 1; j * j <=i; j++) {
+                int square = j * j;
+                dp[i]= Math.min(dp[i], 1+dp[i-square]);
+            }
+        }
+        return dp[n];
+    }
+}
