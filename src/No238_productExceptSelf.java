ead953d5e2fc979b1c3a793ef51d278b0051343a
Kanha gupta <kanhag4163@gmail.com>
2023-09-25 05:27:11 +0530

    No238_productExceptSelf

--- src/No238_productExceptSelf.java
+++ src/No238_productExceptSelf.java
@@ -1,43 +1,23 @@
 public class No238_productExceptSelf {
-        public int[] productExceptSelf(int[] nums){
-        //time complexity O(N) single traversal
-        int[] result=new int[nums.length];
-        int[] prefixProductFromStart=new int[nums.length];
-        int[] prefixProductFromEnd= new int[nums.length];
-
-        prefixProductFromStart[0]=nums[0];
-            for (int i = 1; i < nums.length; i++) {
-                prefixProductFromStart[i]= prefixProductFromStart[i-1]*nums[i];
-            }
-
-            prefixProductFromEnd[nums.length-1]=nums[nums.length-1];
-            for (int i = nums.length-2; i >=0; i--) {
-                prefixProductFromEnd[i]= prefixProductFromEnd[i+1]*nums[i];
-            }
-
-            result[0]=prefixProductFromEnd[1];
-            result[nums.length-1]=prefixProductFromStart[nums.length-2];
-            for (int i = 1; i < nums.length-1; i++) {
-                result[i]= prefixProductFromStart[i-1]* prefixProductFromEnd[i+1];
-            }
-            return result;
+    public int[] productExceptSelf(int[] nums) {
+        int [] left = new int[nums.length];
+        int [] right = new int[nums.length];
+        int [] result = new int[nums.length];
+        int product = 1;
+        for(int i = 0; i<nums.length;i++){
+            product = nums[i] * product;
+            left[i] = product;
+        }
+        product = 1;
+        for(int i = nums.length-1; i>= 0; i--){
+            product = nums[i] * product;
+            right[i]=product;
         }
 
-    public int[] productExceptSelf2(int[] nums) {
-            //time complexity O(N*N) Nested loop
-        int[] result=new int[nums.length];
-
-        for (int i = 0; i <= nums.length-1 ; i++) {
-            result[i]=1;
-            for (int j = 0; j < nums.length; j++) {
-                if(i==j){
-                    if(j==nums.length-1){
-                        break;
-                    }
-                    j++;
-                }
-                result[i]= result[i]*nums[j];
-            }
+        result[0] = right[1]; //since no left array elements
+        result[nums.length-1] = left[nums.length-2]; //since no right array elements left
+        for(int i = 1; i<nums.length-1; i++ ){
+            result[i] = left[i-1] * right[i+1];
         }
         return result;
     }
