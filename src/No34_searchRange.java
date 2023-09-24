bf31bdd2e98081f7a441f86288c1c60799ad1607
Kanha gupta <kanhag4163@gmail.com>
2023-09-25 04:33:21 +0530

    No34_searchRange

--- src/No34_searchRange.java
+++ src/No34_searchRange.java
@@ -1,36 +1,46 @@
 public class No34_searchRange {
     public int[] searchRange(int[] nums, int target) {
-        int[] result={-1,-1};
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
-        //implementing binary search
-        int left=0;
-        int right= nums.length-1;
-        while (left<right){
-            int mid=left + (right - left)/2;
-            if(nums[mid] < target){
-                left = mid + 1;
-            }
-            else {
-                right = mid;
-            }
-        }
-        if(nums[left] != target){
-            return result;
-        }
-        result[0] = left; //first digit index is allocated
-
-        right=nums.length-1; //initiating loop again
-        while (left<right){
-            int mid= left + (right-left+1)/2;
-            if(nums[mid]>target){
-                right=mid-1;
-            } else {
-                left=mid;
-            }
-        }
-        result[1]=right;
+        int [] result = {-1,-1};
+        result[0] = searchLeft(nums, target);
+        result[1]= searchRight(nums, target);
         return result;
+
+    }
+    private int searchLeft(int[] nums, int target){
+        int left = 0;
+        int right = nums.length -1;
+        int index = -1;
+        while(left<= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid]==target){
+                index = mid;
+                right = mid-1;
+            }
+            if (nums[mid]< target){
+                left = mid+1;
+            } else {
+                right = mid -1;
+            }
+        }
+        return index;
+    }
+    private int searchRight(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+        int index = -1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (nums[mid]== target){
+                index = mid;
+                left = mid+1;
+            }
+            else if (nums[mid]< target){
+                left =mid +1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return index;
     }
 }
